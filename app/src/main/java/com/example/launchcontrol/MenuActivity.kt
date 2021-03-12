2803d83c3b37d0aa0392fb051c2fd2e7b20ab754
Icaro-G-Silva <icarogabrielsilva2019@gmail.com>
2021-03-12 14:00:14 -0300

    Updated MainActivity

--- app/src/main/java/com/example/launchcontrol/MenuActivity.kt
+++ app/src/main/java/com/example/launchcontrol/MenuActivity.kt
@@ -4,10 +4,8 @@ import android.annotation.SuppressLint
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.MotionEvent
 import android.view.View
 import android.widget.Button
-import androidx.constraintlayout.widget.Group
 
 class MenuActivity : AppCompatActivity() {
     @SuppressLint("ClickableViewAccessibility")
